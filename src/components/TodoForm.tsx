0744c1481eba18afbea75b39cecf9ec5d391af68
nishiurahiroki@gmail.com <nishiurahiroki@gmail.com>
2022-11-23 04:08:21 +0000

    WIP TodoForm

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,3 +1,24 @@
+'use client';
+
+import { FieldValues, useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+
 export const TodoForm = () => {
-  return <div>Todoフォーム</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        title : <input type="text" {...register('title')} />
+      </div>
+      <div>
+        description : <input type="text" {...register('description')} />
+      </div>
+      <div>
+        <button type="submit">submit</button>
+      </div>
+    </form>
+  );
 };
